2b53a593822ec8faabe5bde151fab507a1ba33d0
Mohammad Zayaan Husain Dulmeer <mzayaan.dulmeer@gmail.com>
2026-03-13 22:23:28 +0400

    Latest Update

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -1,7 +1,48 @@
+const CACHE_NAME = "devintel-cache-v1";
+
+const urlsToCache = [
+"/",
+"/index.html",
+"/js/app.js",
+"/pwa/manifest.json"
+];
+
 self.addEventListener("install", event => {
-console.log("DevIntel Service Worker Installed");
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(urlsToCache))
+
+);
+
 });
 
 self.addEventListener("fetch", event => {
-event.respondWith(fetch(event.request));
+
+event.respondWith(
+
+caches.match(event.request)
+.then(response => {
+
+if (response) return response;
+
+return fetch(event.request)
+.then(networkResponse => {
+
+return caches.open(CACHE_NAME)
+.then(cache => {
+
+cache.put(event.request, networkResponse.clone());
+
+return networkResponse;
+
+});
+
+});
+
+})
+
+);
+
 });
